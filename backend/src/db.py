a42d732110d7255ef0707190f9d30def7434308c
Roi Israeli <roiisra4@gmail.com>
2023-05-29 19:33:48 +0300

    save pull request details in DB

--- backend/src/db.py
+++ backend/src/db.py
@@ -1,12 +1,24 @@
 import os
+import logging
+import sys
 
 from motor.motor_asyncio import AsyncIOMotorClient
 
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
+
+logging_handler = logging.StreamHandler(sys.stdout)
+logging_handler.setLevel(logging.DEBUG)
+logging.getLogger().addHandler(logging_handler)
+
 mongo_client = os.getenv('MONGO_URL')
+logging.info(mongo_client)
+
+
 client = AsyncIOMotorClient(mongo_client)
 db = client.app
-github_log_collection = db.github_logs
+github_log_collection = db.pull_request_logs
 
 
-async def save_github_log():
-    await github_log_collection.insert_one
+async def save_pull_request_log(pull_request_log: dict):
+    result = await github_log_collection.insert_one(pull_request_log)
+    return result.inserted_id
--- backend/src/server.py
+++ backend/src/server.py
@@ -1,17 +1,22 @@
-import json
-from datetime import datetime
+from fastapi import FastAPI, Request, Response, status
 
-from fastapi import FastAPI, Request
+from . import db
 
 app = FastAPI()
 
 
-@app.post('/')
-async def test(request: Request):
-    body = await request.json()
-    now = datetime.now().strftime("%Y%m%d-%H%M%S")
-    with open(f'data/test_res_{now}.json', 'w') as f:
-        json.dump(body, f)
-    print(body)
-    return {}
+@app.post('/', status_code=status.HTTP_201_CREATED)
+async def monitor_pull_request(request: Request, response: Response):
+    github_action_details = await request.json()
+
+    try:
+        pull_request_details = github_action_details['pull_request']
+    except KeyError:
+        response.status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
+        return {'message': 'No Pull Request data is provided in the payload'}
+
+    await db.save_pull_request_log(pull_request_details)
+
+    user = pull_request_details.get("user", {}).get("login", "N/A")
+    return {'message': f'Pull Request was made by user {user} and was saved successfully'}
 
